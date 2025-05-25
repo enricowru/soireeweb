a73564e288bc6986d5c30f1d01dda180a7f382ad
Enrico <enricosuarez777@gmail.com>
2025-05-26 04:55:15 +0800

    Fixed Trusted Origin

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:19006",  # Expo frontend
+    "http://127.0.0.1:3000",   # Web frontend
+    "https://soireeweb.onrender.com",  # For local testing
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:19006",
+    "http://127.0.0.1:3000",
+    "https://soireeweb.onrender.com",
+]
+
+# If needed
+CORS_ALLOW_CREDENTIALS = True
