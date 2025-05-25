33e3a3aaf397dd6cd7268d82f1deb0d05deaf56e
Enrico <enricosuarez777@gmail.com>
2025-05-26 05:17:42 +0800

    CSRF

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def home(request):
     return render(request, 'main.html')
 
+@csrf_exempt
 def login_view(request):
     return render(request, 'login.html')
