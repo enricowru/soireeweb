7d7af5b98d0c05ba39db49783b55de0d99957321
enricowru <enricosuarez777@gmail.com>
2025-06-04 03:18:55 +0800

    Update views.py

--- main/views.py
+++ main/views.py
@@ -314,7 +314,9 @@ def approve_review(request, review_id):
 @csrf_exempt
 @require_http_methods(["POST"])
 def signup(request):
-    try:
+    try:   
+        return JsonResponse({'message': 'TEST', 'error': 'conflict'}, status=500)
+
         data = json.loads(request.body)
         firstname = data.get('firstname')
         lastname = data.get('lastname')
