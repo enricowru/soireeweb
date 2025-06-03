ce00eacccebce4389ee40d240d2e1af3f9919c93
enricowru <enricosuarez777@gmail.com>
2025-06-04 01:21:56 +0800

    Update utils.py web images cloud integration

--- main/utils.py
+++ main/utils.py
@@ -1,12 +1,14 @@
 from fuzzywuzzy import process
-import re
 from django.conf import settings
-import os
 from django.core.cache import cache
-import requests
 from PIL import Image
 from io import BytesIO
 from django.templatetags.static import static
+
+import re
+import os
+import requests
+import cloudinary
 
 def get_fuzzy_match(query, choices, threshold=80):
     """
@@ -155,46 +157,77 @@ def get_all_theme_images(theme_name):
 
 def get_theme_images(theme):
     """Helper function to get images based on theme"""
-    themes = {
+    # Use Cloudinary
+    themes_public_ids = {
+        
         # Disney & Princess Themes
-        'mickey': ['https://drive.google.com/uc?export=view&id=1dhdAf0ztwqbJJ_m1dvINkyuSrq0afkOr', 'https://drive.google.com/uc?export=view&id=1eetx4_dwqVvJBs9J4hgOePO31zTAxbpw', 'https://drive.google.com/uc?export=view&id=1NmtHS5Fbso17RfMy8iQqqsFirMqU2gpg'],
-        'frozen': ['https://drive.google.com/uc?export=view&id=1SzA3O-Dpp_DJnAYyzVUgI4HH-dsEfZ7G', 'https://drive.google.com/uc?export=view&id=13E-0EnaG6m53O0kDOrFywHgptrnIykoU', 'https://drive.google.com/uc?export=view&id=1XmDYigGKYAk1ShI6ek_0X4ICZTKkroS9'],
-        'cinderella': ['https://drive.google.com/uc?export=view&id=11427oy9btqMSkz4_joZ3N-tF3XV-D1Ws', 'https://drive.google.com/uc?export=view&id=1IaRp9n2ZWahIiSs3BZoDjutz6VE_PjF8', 'https://drive.google.com/uc?export=view&id=19jiaXpLCmv2xNqF0Oi2J7AQp3BkMmCS9'],
-        'snow white': ['https://drive.google.com/uc?export=view&id=1se2t4kO45UxO62JY8lmbzaKedyhOcTaf', 'https://drive.google.com/uc?export=view&id=1trggQTDOnglh0zAFPIDvego80Xy-sMi4', 'https://drive.google.com/uc?export=view&id=1pjgosC_EA-AdMvo-GlM-7tRw8q9CXkMC'],
-        'tinkerbell': ['https://drive.google.com/uc?export=view&id=1WeN76Dz7vnD3DSwESdlYszr8jg6Au9Rr', 'https://drive.google.com/uc?export=view&id=1FGK6Og5AxSSPhoX5djrEPSfQ7edB9whE', 'https://drive.google.com/uc?export=view&id=1E_hS4MX05acvtjAQa_La-IauUiKcC2BG'],
-        'princess': ['https://drive.google.com/uc?export=view&id=19Ht3rT8VHTIcIKrftsb6FgU9WrBkOcLn', 'https://drive.google.com/uc?export=view&id=1EMqzqLT-d2l8f-TRtlSymUAcYW__32jG', 'https://drive.google.com/uc?export=view&id=15_q1CWv853Hl4Z56z5OmN8Nu_M8FH4s9'],
+        'mickey': ['mickey1_tjr7h0', 'mickey2_pxslgt', 'mickey3_qiya8u'],
+        'frozen': ['frozen1_dk7v5o', 'frozen2_tyo4wh', 'frozen3_juo1gl'],
+        'cinderella': ['cinderella1_mmllks', 'cinderalla2_je5abg', 'cinderella3_ryewrw'],
+        'snow white': ['snowwhite1_ebjhlv', 'snowwhite2_xoq7wp', 'snowwhite3_qgghb6'],
+        'tinkerbell': ['tinkerbell1_wutjm9', 'tinkerbell2_rvusmy', 'tinkerbell3_x1vztm'],
+        'princess': ['princess1_ckf2ef', 'princess2_rn0z7q', 'princess3_flsdje'],
+        
         # Movie & Animation Themes
-        'incredibles': ['https://drive.google.com/uc?export=view&id=1_OEzdaWNCYrhQhFjlNynkmIHzEcQ4NpL', 'https://drive.google.com/uc?export=view&id=12FW6tmbYLYyT1_lplY8tZkJEDMlzL9EN', 'https://drive.google.com/uc?export=view&id=1MKSPs4GRdIwaCGa3SybnL7Xa5P8dnqoR'],
-        'spongebob': ['https://drive.google.com/uc?export=view&id=11mFVUtmr8FFHhVhMqpk435BCPotr1l0K', 'https://drive.google.com/uc?export=view&id=1-dr_QJ7oplmydPUYFjCiNpF-pdiHOKcK', 'https://drive.google.com/uc?export=view&id=1-IxCqn3t2rl2FuxYJVBKf9YoxWqjA_B8', 'https://drive.google.com/uc?export=view&id=1ftm_MMj8a--liDM3kgFZ34xaZ-pXhJ4n'],
-        'nemo': ['https://drive.google.com/uc?export=view&id=18QsWaJC0Pn7pHWMwRjS5L2QmN1QFPtOh', 'https://drive.google.com/uc?export=view&id=1Hky-DKlwqv4dMEbDMmVjkZ7thTEIWt4G', 'https://drive.google.com/uc?export=view&id=132ApVHciz3qkRCDzX0Jcb9SHJ1MEBGwn'],
-        'cars': ['https://drive.google.com/uc?export=view&id=1Wd_z4XCG1qU6Pz9tzYLNfPlxtcUlxyC9', 'https://drive.google.com/uc?export=view&id=1KJ5UvyDzsLJ-oMDSC59b5VoUQWTcV82x', 'https://drive.google.com/uc?export=view&id=1RTvslZGJui084JyKwAEXIhFEaCLEY7lG'],
+        'incredibles': ['incredibles1_j3ecdz', 'incredibles2_t6woik', 'incredibles3_rsnody'],
+        'spongebob': ['spongebob1_jygh8a', 'spongebob2_nhnz7r', 'spongebob3_lnksll', 'spongebob4_z3pwmv'],
+        'nemo': ['nemo1_fwlhqc', 'nemo2_csahmk', 'nemo3_ffr1bz'],
+        'cars': ['cars1_m5dywg', 'cars2_owb6uf', 'cars3_vlbluf'],
+        
         # Gaming & Entertainment
-        'mario': ['https://drive.google.com/uc?export=view&id=1g2deztjMFVD8k9DJzz4ScJq0ib9NqxKE', 'https://drive.google.com/uc?export=view&id=1nSvX6En9Vu263JiVMXp0NOAZtqh3iHSV', 'https://drive.google.com/uc?export=view&id=1VafctPCR99iXswof3ZiosXaECKEXe1c0'],
-        'roblox': ['https://drive.google.com/uc?export=view&id=14-X_S4h1McyFBQNfIzQ3tracxItLEMj4', 'https://drive.google.com/uc?export=view&id=1TnAjZwGyP5MhmiT8YhhaG7Vs-YTMdwo2', 'https://drive.google.com/uc?export=view&id=1GrvnOHUhnTPEb_l7Mo0BSA5Q1FAHeP30'],
-        'one piece': ['https://drive.google.com/uc?export=view&id=1ZAtu9-BEwaJ45seXSMEtZbLgZe7wCpfB', 'https://drive.google.com/uc?export=view&id=1yZWmrf2bAvP40QR2cOCwAlStl7AaCurR', 'https://drive.google.com/uc?export=view&id=1Yz4TjJ_ODtKlZfA-RToBnVKZAxx_upgN'],
+        'mario': ['mario1_htowh3', 'mario2_nhrpwo', 'mario3_wxhbpr'],
+        'roblox': ['roblox1_g3jscs', 'roblox2_hpso4g', 'roblox3_pkrmmz'],
+        'one piece': ['onepiece1_ihxymg', 'onepiece2_kqlelt', 'onepiece3_gixekh'],
+        
         # Superhero & Action
-        'avengers': ['https://drive.google.com/uc?export=view&id=1x1H4lfiHCS9gujCTINritStf1em4cpIL', 'https://drive.google.com/uc?export=view&id=1ykdtjFQrqHsqriZlZwijqLld-fc3SovO', 'https://drive.google.com/uc?export=view&id=1HQwupeAc8-uxOT-kZWheJIPJ1fKqzqf8'],
-        'transformers': ['https://drive.google.com/uc?export=view&id=1lWXXqfQ93-VZyu4T5qDU9kWO6Uhi-pS2', 'https://drive.google.com/uc?export=view&id=1O56IZiuGosc8nTVWa0gtzSpBCQq9_EKJ', 'https://drive.google.com/uc?export=view&id=19RB2Gj0RXQhrLJjCl9s1eqzrneJvOsyP'],
+        'avengers': ['avengers1_z8kgd7', 'avengers2_kgeegl', 'avengers3_rp9hxm'],
+        'transformers': ['transformers1_hpi1nw', 'transformers2_yrvfhz', 'transformers3_jkxnmk'],
+        
         # Sports & Vehicles
-        'basketball': ['https://drive.google.com/uc?export=view&id=1PM6_dQp16rLnQtbeS0qP74OLFUY09j4C', 'https://drive.google.com/uc?export=view&id=1y1rMVxungHVGRH8b5DdWFaD8CW7iDxE0', 'https://drive.google.com/uc?export=view&id=1NCLGKJ8QHTrvFRsAF3Lu-9oQ0izHcP6N'],
-        'ferrari': ['https://drive.google.com/uc?export=view&id=14pHChbvHjrG7pVrYuoiF8orOWtvr0b-f', 'https://drive.google.com/uc?export=view&id=1TjfHVkODw1KiaECD3ds70sbBkXEN5mfq', 'https://drive.google.com/uc?export=view&id=1XVvWdrI8Xbk6oU2iEPoJMSFTeIFvsQwa'],
+        'basketball': ['bball1_lzy501', 'bball2_zmutfl', 'bball3_u7hgin'],
+        'ferrari': ['ferrari1_gjoxg9', 'ferrari2_bnyapi', 'ferrari3_oqh4xi'],
+        
         # Nature & Animals
-        'zoo': ['https://drive.google.com/uc?export=view&id=1GMrc1myk7hMC0Xy2UVINxyFz6OPN2j1m', 'https://drive.google.com/uc?export=view&id=1KGSrVIxxeLT-_LzlA0yZqz4ffFdQX2uz', 'https://drive.google.com/uc?export=view&id=1GLlNO9HhfUnmQstz6BoVGMirk5__fNMj'],
-        'dinosaur': ['https://drive.google.com/uc?export=view&id=107l1t3L1qN6tfhWJzCSIyPWs5cJU_169', 'https://drive.google.com/uc?export=view&id=1xrGu4Avwrhgrk8B8KtaUhKqgSBSeK6GU', 'https://drive.google.com/uc?export=view&id=18hhAof80JzQDhCt_lkmTNNRpfL9mI8S9'],
-        'bear': ['https://drive.google.com/uc?export=view&id=1yd6j8cTVMok4zWAbtSGpO-IiXPUbqD7j', 'https://drive.google.com/uc?export=view&id=18TdB1oahUnAgs1_34hhQ2Pbh60Lxw34E', 'https://drive.google.com/uc?export=view&id=1F3IPCQTSdrwHi-RPJTZDAU6vCf_vSdVG'],
-        'butterfly': ['https://drive.google.com/uc?export=view&id=1OcKpW2MQJuFJRm1G7Goo69XxAbyI2LQR', 'https://drive.google.com/uc?export=view&id=1O0C_v3IrBMyzmmoUZH35ozzSOwSpcMAM', 'https://drive.google.com/uc?export=view&id=1R2rgNL9Saf_MBnLV32mLCI-1bz23FyK3'],
+        'zoo': ['zoo1_lkhguo', 'zoo2_izmxug', 'zoo3_tpivyn'],
+        'dinosaur': ['dino1_rgtkxl', 'dino2_i21hjp', 'dino3_emjiim'],
+        'bear': ['bear1_h4p6et', 'bear2_bliufl', 'bear3_uyhzr7'],
+        'butterfly': ['butterfly1_jnpau7', 'butterfly2_o3gnyl', 'butterfly3_g2eouo'],
+        
         # Colors & Styles
-        'pink': ['https://drive.google.com/uc?export=view&id=1qHxE-rVD9rXkeTFz-p-brhGblwzQ2HDM', 'https://drive.google.com/uc?export=view&id=19cJtYM6bRMK5rzC-F-za7_DE1jY4F56o', 'https://drive.google.com/uc?export=view&id=1WY24TKweSXq_lFd9xIA4k8pjrF5JK_12', 'https://drive.google.com/uc?export=view&id=1lhFhY7hVDt1oRfZODU1sZsjZv62KXbSs'],
-        'green': ['https://drive.google.com/uc?export=view&id=1On07tStA7vhArHJ256iWciV2308rSGW_', 'https://drive.google.com/uc?export=view&id=1gtIVwpxK0ynfLF3P1y20Mptrz1O7dZvO', 'https://drive.google.com/uc?export=view&id=1b9XDzoZGiMDwkbZ0MZ16bWcKwDc76uxD'],
-        'rainbow': ['https://drive.google.com/uc?export=view&id=1V9a_nYpZu2WpjK4G7zXCU-FGjQxX-_NR', 'https://drive.google.com/uc?export=view&id=10fsShHr2cBK03QQ4iUCqKCI4HJlQ6jo3', 'https://drive.google.com/uc?export=view&id=1tlpkr73HtKFHBOI10hM53aSs1IufkwrD'],
+        'pink': ['pink1_xpydyr', 'pink2_zws6c6', 'pink3_du7zpo', 'pink4_zg4mno'],
+        'green': ['green1_fkl6x9', 'green2_fxbdhw', 'green3_mzeklc'],
+        'rainbow': ['rainbow1_pqsbfi', 'rainbow2_be8qcj', 'rainbow3_pf0k0c'],
+        
         # Special Themes
-        'halloween': ['https://drive.google.com/uc?export=view&id=1_kF3DjwPT5fVqpwoQsfNpzSwUQoMX_sh', 'https://drive.google.com/uc?export=view&id=12TQHS6NHhHTTGeAs-iJBOpbP--SI3URo', 'https://drive.google.com/uc?export=view&id=1ZLJ57QFXOf135fb1twEFTrNsWBReA3aJ'],
-        'wedding': ['https://drive.google.com/uc?export=view&id=1P326W7m_Ebhberok8i3ryNB3abZBU5oC', 'https://drive.google.com/uc?export=view&id=1QBbrtgPyF3RI4YVnJ8DYFwFw4HKnw4f_', 'https://drive.google.com/uc?export=view&id=1oTYVV7uBCoeVBZ1CY56kkrFvErh7dPmu'],
-        'traditional': ['https://drive.google.com/uc?export=view&id=18n2D128-UCjOH_VN1jN8TUq2s4j2ttrN', 'https://drive.google.com/uc?export=view&id=1s-P3EjJkbjRhPX90UfNyWYUBGN9O57QR', 'https://drive.google.com/uc?export=view&id=1f_juko83Y2H5RJj6SbrI_GUUDtKtF4EF'],
+        'halloween': ['halloween1_x4onph', 'halloween2_tp5kqb', 'halloween3_xaypho'],
+        'wedding': ['wedding1_i4j4ed', 'wedding2_ahxv8y', 'wedding3_ebx0yl'],
+        'traditional': ['traditional1_fdjojh', 'traditional2_v0a3un', 'traditional3_e2c2vc'],
+        
         # Other Popular Themes
-        'barbie': ['https://drive.google.com/uc?export=view&id=1gV8HjfPiudDXsvmlDrkHNr9v9GhGk9YF', 'https://drive.google.com/uc?export=view&id=18Uph-YVMvttV8NkxrZKPzpJ-lDTYrW1x', 'https://drive.google.com/uc?export=view&id=1iPJqFkoFu4Yr12FlwdLx09au8tchYjuW'],
-        'kuromi': ['https://drive.google.com/uc?export=view&id=1FojdkqjxeIJ48Oizo_i-VLVg5Kc-mt85', 'https://drive.google.com/uc?export=view&id=1LJcmdWWgO_oWlt6PGHPnMMCOjfJUNggK', 'https://drive.google.com/uc?export=view&id=1whqqvRj50QDnc7d9asDYsBWaReBrGpSf'],
-        'cocomelon': ['https://drive.google.com/uc?export=view&id=1QCMLBwmlEaygJ_fdLufzIr8nQWVcbqe1', 'https://drive.google.com/uc?export=view&id=1i1W7SOPME6Sf4jyA3vb-wOW38X89i_ft', 'https://drive.google.com/uc?export=view&id=1rw4vCw0lfneqqNR_EPwCnX240d8elqRR'],
-        'pony': ['https://drive.google.com/uc?export=view&id=1YZ_xEc2AjgIpCkpw_35Je6RAFNHYcKel', 'https://drive.google.com/uc?export=view&id=13zlS-cD047oommzheqHgjG8SevfGa_67', 'https://drive.google.com/uc?export=view&id=1ol1ZoHZ5cRZd2xFxl3LhQ2LoCTkna8Vk'],
+        'barbie': ['barbie1_y2kro4', 'barbie2_adipbp', 'barbie3_wmmwam'],
+        'kuromi': ['kuromi1_eoqwpc', 'kuromi2_ielfn3', 'kuromi3_nchbhx'],
+        'cocomelon': ['cocolemon1_xvkaxa', 'cocomelon2_x8gwf6', 'cocomelon3_bjw2xa'],
+        'pony': ['pony1_rdvceu', 'pony2_u9xona', 'pony3_idj8mn'],
     }
-    return themes.get(theme.lower().strip(), [])
+
+    theme_public_ids = themes_public_ids.get(theme.lower().strip(), [])
+
+    image_urls = [cloudinary.utils.cloudinary_url(public_id, secure=True)[0] for public_id in theme_public_ids]
+
+    return image_urls
+
+CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME', 'dzjrdqkiw')
+CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY', '891881498673297')
+CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET', 'Z7QaNRs-E_qgvkCByZxev7fwyiU')
+
+# Optional: Configure as default storage for Django's collectstatic
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaStorage'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticStorage'
+
+# Configure Cloudinary directly (alternative to CLOUDINARY_URL environment variable)
+cloudinary.config(
+    cloud_name = CLOUDINARY_CLOUD_NAME,
+    api_key = CLOUDINARY_API_KEY,
+    api_secret = CLOUDINARY_API_SECRET,
+    secure = True # Use HTTPS
+)
