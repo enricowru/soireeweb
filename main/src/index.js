a21143653243ba1ff3bf0a620a026fa93453e0f6
enricowru <enricosuarez777@gmail.com>
2025-06-05 00:38:01 +0800

    Update index.js

--- main/src/index.js
+++ main/src/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const reviewRoutes = require('./routes/review');
+app.use('/reviews', reviewRoutes);
+
 
 const app = express();
 
