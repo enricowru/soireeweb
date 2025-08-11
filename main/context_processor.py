21a4b48d0307c263fc1b66f215809370facdd1d4
JohnDanielTejero <96475894+JohnDanielTejero@users.noreply.github.com>
2025-08-11 21:57:44 +0800

    issue found: BASE_URL is always pointing to local, context processor also updated

--- main/context_processor.py
+++ main/context_processor.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
+
 def global_settings(request):
     return {
-        'PROD_HOST': settings.PROD_HOST,
-        'LOCAL_HOST': settings.LOCAL_HOST,
+        "BASE_URL": settings.BASE_URL
     }
